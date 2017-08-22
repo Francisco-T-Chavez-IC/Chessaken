10ee720c7cb94d0eb764e10776d3dd1f5cf799da
Francisco T. Chavez <francisco.tx.chavez@intel.com>
2017-08-21 17:48:39 -0700

    Updating Formatting
    I updated some of the formatting of the MainCamera and the Board script a bit as I looked them over.

--- Assets/Scripts/UI/MainCamera.cs
+++ Assets/Scripts/UI/MainCamera.cs
@@ -1,15 +1,24 @@
-﻿using UnityEngine;
+﻿
 using System.Collections;
 
-public class MainCamera : MonoBehaviour {
-    private float rotation; // Euler's "y" rotation
+using UnityEngine;
 
-    void Update() {
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, rotation, 0), Time.deltaTime * 4f);
-    }
 
-    public void changeTeam(int team) {
-        // Set rotation depending of the current turn team
-        rotation = (team == -1) ? 0 : 180;
-    }
+public class MainCamera 
+	: MonoBehaviour
+{
+	private float rotation; // Euler's "y" rotation
+	
+	void Update()
+	{
+		transform.rotation = Quaternion.Slerp(transform.rotation, 
+											  Quaternion.Euler(0, rotation, 0), 
+											  Time.deltaTime * 4f);
+	}
+	
+	public void changeTeam(int team)
+	{
+		// Set rotation depending of the current turn team
+		rotation = (team == -1) ? 0 : 180;
+	}
 }
